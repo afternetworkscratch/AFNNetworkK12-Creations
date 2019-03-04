18c3ed18122f1f0418caaae74822460c612998ae
After <45772308+AfterNetwork@users.noreply.github.com>
2019-03-04 11:22:30 -0800

    Update JavaScriptTesting.js

--- Unreleased/JavaScriptTesting.js
+++ Unreleased/JavaScriptTesting.js
@@ -1,3 +1,6 @@
 console.log('This is the sending console logs.');
 // This is a comment.
 console.log(95 + 5);
+console.log('Teaching the world how to code'.length)
+console.log('    Sure...   '.trim());
+console.log(Math.random() * 100)
