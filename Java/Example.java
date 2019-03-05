79078ae56dfa3ba1bb9a474be44134d21d32dcac
After <45772308+AfterNetwork@users.noreply.github.com>
2019-03-05 09:34:09 -0800

    Java

---
+++ Java/Example.java
@@ -0,0 +1,10 @@
+System.out.printIn("This is an example of how the output would show.");
+System.out.printIn(1);
+System.out.printIn("An 'int' data type")
+System.out.printIn(true)
+System.out.printIn("That is a boolean.")
+System.out.printIn('H')
+System.out.printIn("Another data type, char.")
+int Number = 16793;
+boolean Test = true;
+char AtoF = 'A';
