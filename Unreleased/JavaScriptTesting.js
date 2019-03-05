5690dbe5cbdf2e752271118ab6f774b783a6a712
After <45772308+AfterNetwork@users.noreply.github.com>
2019-03-05 09:49:32 -0800

    Update JavaScriptTesting.js

--- Unreleased/JavaScriptTesting.js
+++ Unreleased/JavaScriptTesting.js
@@ -4,3 +4,6 @@ console.log(95 + 5);
 console.log('Teaching the world how to code'.length)
 console.log('    Sure...   '.trim());
 console.log(Math.random() * 100)
+var Test = 'This is a test, i may make this a JS based respiratory soon.';
+console.log(Test)
+// Right now, i am just working on stuff, i may create some scratch experimental extensions on scratchX (https://scratchx.org/)
